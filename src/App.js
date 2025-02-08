9b6b532686bdd5ed0251c0bf935088afa45f7b67
Hans Salangsang <hans.salangsang@gmail.com>
2025-02-08 15:38:15 +0800

    Changed position of spline background

--- src/App.js
+++ src/App.js
@@ -211,7 +211,7 @@ function App() {
               left: "0%",
               width: "100%",
               height: "100%",
-              zIndex: 0,
+              zIndex: -1,
               pointerEvents: "auto",
             }}
           >
